d4eaa2add02027f7e7fff030992f3061cdbce578
İmdat Gürbüz <grbzimdatofficial@gmail.com>
2023-04-21 14:27:56 +0300

    first template

--- bmi-calculator.py
+++ bmi-calculator.py
@@ -1,1 +1,28 @@
+import tkinter as tk
 
+screen = tk.Tk()
+screen.minsize(220,260)
+screen.title("BMI Calculator")
+screen.config(background="#EDF1D6",)
+
+#? BMI Calculator label
+first_label = tk.Label(text="BMI Calculator",bg="#EDF1D6",padx=10,pady=5,fg="#40513B",font="Times 16 bold")
+first_label.pack()
+
+#? height
+height_label = tk.Label(text="Enter height(cm): ",background="#EDF1D6",pady=10)
+height_entry = tk.Entry()
+height_label.pack()
+height_entry.pack()
+
+#? weight
+weight_label = tk.Label(text="Enter weight(kg)",background="#EDF1D6",pady=10)
+weight_entry = tk.Entry()
+weight_label.pack()
+weight_entry.pack()
+
+#?calculate button
+calculate_button = tk.Button(text="Calculate")
+calculate_button.pack(padx=10,pady=10)
+
+tk.mainloop()
